d2454d4c0958ddb6ae4c1f1919be047991da2690
pon32258 <pon1852@hotmail.com>
2020-02-02 08:11:15 +0700

    [Add] route water

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const firebase = require("firebase");
 require("firebase/database");
 
 const firebaseConfig = {
-
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    storageBucket: process.env.STORAGEBUCKET
 };
 
 firebase.initializeApp(firebaseConfig);
